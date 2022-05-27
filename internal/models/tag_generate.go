76c4433431d97b74b6d87174e47ad6cb26362a27
Lukas Zapletal <lzap+git@redhat.com>
2022-05-27 14:31:27 +0200

    Simplify transactions
    Let's make a decision that will simplify transactions - they can only be done in dao functions. Although it is convenient to use SQL transactions in the service package for example for a whole REST API call, long-lived transactions is something that should not be abused. This patch refactors the helper WithTransaction function in a way that it can be only used within a dao implementation (e.g. sqlx). Error compensation is used to delete resource when EC2 AWS upload operation fails.

---
+++ internal/models/tag_generate.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+func randomBase62(bytes int) string {
+	b := make([]byte, bytes)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic("unable to read pseudorandom numbers")
+	}
+	n := new(big.Int)
+	n.SetBytes(b)
+	return n.Text(62)
+}
+
+// GenerateTag sets Tag field to a random base62 (base64 without / or +) with a length
+// of 20 characters. Values are cryptographically pseudo-random and pseudo-unique with
+// probability of getting same values the same as for random UUID.
+func GenerateTag() string {
+	// generate from more bytes because fixed-size base62 cannot be guaranteed at this size
+	return randomBase62(20)[0:20]
+}
