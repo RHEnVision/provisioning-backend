dcedabcb7e90ac15b97d2439d5f8123b91b55434
Ondrej Ezr <oezr@redhat.com>
2022-05-29 23:11:15 +0200

    Environment aware config initialization
    Refs: HMSPROV-77

---
+++ internal/config/5-devel_config.go
@@ -0,0 +1,27 @@
+//go:build !test && !production
+// +build !test,!production
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetDefault("featureFlags.environment", "developement")
+
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("defaults")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	viper.SetConfigName("local")
+	err = viper.MergeInConfig()
+	if err != nil {
+		fmt.Println("Could not read local.yaml", err)
+	}
+}
