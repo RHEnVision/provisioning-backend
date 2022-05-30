c27f670bcb18f1ef70b552facbdc75dd58c68686
Ondrej Ezr <oezr@redhat.com>
2022-05-30 11:14:34 +0200

    Setup clowder in real config

--- internal/config/5_real_config.go
+++ internal/config/5_real_config.go
@@ -6,22 +6,36 @@ package config
 import (
 	"fmt"
 
+	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
 	"github.com/spf13/viper"
 )
 
 func init() {
-	viper.SetDefault("featureFlags.environment", "developement")
+	if clowder.IsClowderEnabled() {
+		cfg := clowder.LoadedConfig
 
-	viper.AddConfigPath("./configs")
-	viper.SetConfigName("defaults")
+		viper.Set("featureFlags.environment", "production")
+		viper.Set("database.host", cfg.Database.Hostname)
+		viper.Set("database.port", cfg.Database.Port)
+		viper.Set("database.user", cfg.Database.Username)
+		viper.Set("database.password", cfg.Database.Password)
+		viper.Set("database.name", cfg.Database.Name)
+		viper.Set("prometheus.port", cfg.MetricsPort)
+		viper.Set("prometheus.path", cfg.MetricsPath)
+	} else {
+		viper.SetDefault("featureFlags.environment", "developement")
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-	viper.SetConfigName("local")
-	err = viper.MergeInConfig()
-	if err != nil {
-		fmt.Println("Could not read local.yaml", err)
+		viper.AddConfigPath("./configs")
+		viper.SetConfigName("defaults")
+
+		err := viper.ReadInConfig()
+		if err != nil {
+			panic(err)
+		}
+		viper.SetConfigName("local")
+		err = viper.MergeInConfig()
+		if err != nil {
+			fmt.Println("Could not read local.yaml", err)
+		}
 	}
 }
