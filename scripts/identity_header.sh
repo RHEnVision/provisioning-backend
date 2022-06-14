cc42e611a89b1c76ef79d8cb3013438ef23e3dbd
Ondrej Ezr <oezr@redhat.com>
2022-06-14 16:08:07 +0200

    Add type to identity header generator

--- scripts/identity_header.sh
+++ scripts/identity_header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ENC=`echo "{\"identity\": {\"account_number\":\"$1\", \"internal\":{\"org_id\":\"$2\"}}}" | base64 -w0`
+ENC=`echo "{\"identity\": {\"type\": \"User\", \"account_number\":\"$1\", \"internal\":{\"org_id\":\"$2\"}}}" | base64 -w0`
 echo "x-rh-identity: $ENC"
