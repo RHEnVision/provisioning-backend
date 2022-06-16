fa639304de8ca984d1159a9d6768b730e1237e4e
Ondřej Ezr <oezr@redhat.com>
2022-06-16 09:36:31 +0200

    Add pubkey create tests (#36)
    Add Create pubkey service test

--- internal/dao/stubs/pubkey_dao.go
+++ internal/dao/stubs/pubkey_dao.go
@@ -19,36 +19,49 @@ func init() {
 	dao.GetPubkeyDao = getPubkeyDao
 }
 
-type ContextReadError struct{}
+type contextReadError struct{}
 
-func (m *ContextReadError) Error() string {
+func (m *contextReadError) Error() string {
 	return "failed to find or convert dao stored in testing context"
 }
 
 func WithPubkeyDao(parent context.Context, init_store []*models.Pubkey) context.Context {
-	ctx := context.WithValue(parent, pubkeyCtxKey, PubkeyDaoStub{init_store})
+	ctx := context.WithValue(parent, pubkeyCtxKey, &PubkeyDaoStub{init_store})
 	return ctx
 }
 
-func getPubkeyDao(ctx context.Context) (dao.PubkeyDao, error) {
-	var err error
-	pkdao, ok := ctx.Value(pubkeyCtxKey).(dao.PubkeyDao)
-	if !ok {
-		err = &ContextReadError{}
+func PubkeyStubCount(ctx context.Context) (int, error) {
+	pkdao, err := getPubkeyDaoStub(ctx)
+	if err != nil {
+		return 0, err
 	}
+	return len(pkdao.store), nil
+}
 
+func getPubkeyDao(ctx context.Context) (dao.PubkeyDao, error) {
+	return getPubkeyDaoStub(ctx)
+}
+
+func getPubkeyDaoStub(ctx context.Context) (pkdao *PubkeyDaoStub, err error) {
+	var ok bool
+	if pkdao, ok = ctx.Value(pubkeyCtxKey).(*PubkeyDaoStub); !ok {
+		err = &contextReadError{}
+	}
 	return pkdao, err
 }
 
-func (PubkeyDaoStub) Create(ctx context.Context, pk *models.Pubkey) error { return nil }
-func (PubkeyDaoStub) CreateWithResource(ctx context.Context, pk *models.Pubkey, pkr *models.PubkeyResource) error {
+func (mock *PubkeyDaoStub) Create(ctx context.Context, pk *models.Pubkey) error {
+	mock.store = append(mock.store, pk)
 	return nil
 }
-func (PubkeyDaoStub) Update(ctx context.Context, pk *models.Pubkey) error { return nil }
-func (mock PubkeyDaoStub) GetById(ctx context.Context, id uint64) (*models.Pubkey, error) {
+func (*PubkeyDaoStub) CreateWithResource(ctx context.Context, pk *models.Pubkey, pkr *models.PubkeyResource) error {
+	return nil
+}
+func (*PubkeyDaoStub) Update(ctx context.Context, pk *models.Pubkey) error { return nil }
+func (mock *PubkeyDaoStub) GetById(ctx context.Context, id uint64) (*models.Pubkey, error) {
 	return mock.store[0], nil
 }
-func (mock PubkeyDaoStub) List(ctx context.Context, limit, offset uint64) ([]*models.Pubkey, error) {
+func (mock *PubkeyDaoStub) List(ctx context.Context, limit, offset uint64) ([]*models.Pubkey, error) {
 	return mock.store, nil
 }
-func (PubkeyDaoStub) Delete(ctx context.Context, id uint64) error { return nil }
+func (*PubkeyDaoStub) Delete(ctx context.Context, id uint64) error { return nil }
--- internal/services/pubkey_service_test.go
+++ internal/services/pubkey_service_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -29,8 +30,7 @@ func buildPkStore() []*models.Pubkey {
 }
 
 func TestListPubkeysHandler(t *testing.T) {
-	ctx := context.Background()
-	ctx = identity.WithIdentity(t, ctx)
+	ctx := identity.WithIdentity(t, context.Background())
 	ctx = dao_stubs.WithPubkeyDao(ctx, buildPkStore())
 
 	req, err := http.NewRequestWithContext(ctx, "GET", "/api/provisioning/pubkeys", nil)
@@ -51,4 +51,33 @@ func TestListPubkeysHandler(t *testing.T) {
 	}
 
 	assert.Equal(t, 2, len(pubkeys), "expected two pubkeys in response json")
+}
+
+func TestCreatePubkeyHandler(t *testing.T) {
+	var err error
+	var json_data []byte
+	ctx := dao_stubs.WithPubkeyDao(context.Background(), nil)
+
+	values := map[string]interface{}{
+		"account_id": 1,
+		"name":       "very cool key",
+		"body":       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhnn80ZywmjeBFFOGm+cm+5HUwm62qTVnjKlOdYFLHN lzap",
+	}
+
+	if json_data, err = json.Marshal(values); err != nil {
+		t.Fatal("unable to marshal values to json")
+	}
+	req, err := http.NewRequestWithContext(ctx, "POST", "/api/provisioning/pubkeys", bytes.NewBuffer(json_data))
+	assert.Nil(t, err, fmt.Sprintf("Error creating a new request: %v", err))
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CreatePubkey)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code, "Handler returned wrong status code")
+
+	storecnt, err := dao_stubs.PubkeyStubCount(ctx)
+	assert.Nil(t, err, fmt.Sprintf("Error reading stub count: %v", err))
+	assert.Equal(t, 1, storecnt, "Pubkey has not been Created through DAO")
 }
