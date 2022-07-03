b7f7f900e1b8a8b1d3c1cb6b3609289ae102a2d1
Lukáš Zapletal <lzap+git@redhat.com>
2022-07-03 11:32:40 +0200

    Make identity script MacOS compatible (#70)

--- scripts/identity_header.sh
+++ scripts/identity_header.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+#
+# Generates Red Hat identity header and plain HTTP authorization.
+#
+une="$(uname -s)"
+case "${une}" in
+  Linux*)     B64OPTS="-w0";;
+  Darwin*)    B64OPTS="-b0";;
+  *)          echo "Unknown system ${une}" && exit 1
+esac
 
-ENC=`echo "{\"identity\": {\"type\": \"User\", \"account_number\":\"$1\", \"internal\":{\"org_id\":\"$2\"}}}" | base64 -w0`
-echo "x-rh-identity: $ENC"
+if [ -n "${1}" ] && [ -n "${2}" ]; then
+  IDENTITY=$(echo "{\"identity\": {\"type\": \"User\", \"account_number\":\"$1\", \"internal\":{\"org_id\":\"$2\"}}}" | base64 $B64OPTS)
+  echo "X-RH-Identity: $IDENTITY"
+fi
+
+if [ -n "${3}" ] && [ -n "${4}" ]; then
+  echo "Authorization: Basic $(echo "$3:$4\c" | base64 $B64OPTS)"
+fi
