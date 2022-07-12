a6bc829d7fa0d38d00217cfb13d69f0b3a00972d
Ondrej Ezr <oezr@redhat.com>
2022-07-12 23:15:18 +0200

    Move AppTypeId to sources package
    AppType needs to be lazy loaded and is only static in test environment,
    so it makes no sense to keep it in global config.

--- internal/clients/sources/sources_errors.go
+++ internal/clients/sources/sources_errors.go
@@ -8,3 +8,6 @@ var MoreThenOneAuthenticationForSourceErr = errors.New("more then one authentica
 var AuthenticationForSourcesNotFoundErr = errors.New("authentications for source weren't found in sources app")
 var SourcesClientErr = errors.New("sources client error")
 var ApplicationNotFoundErr = errors.New("application not found is sources app")
+var ApplicationTypesFetchUnsuccessful = errors.New("failed to fetch ApplicationTypes")
+var ApplicationTypeNotFound = errors.New("application type 'provisioning' has not been found in types supported by sources")
+var ApplicationTypeCacheFailed = errors.New("application type id failed to write to cache")
