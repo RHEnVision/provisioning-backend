d7544df0f63831ec53d06f4ddaa03c1b04ea9dfb
Ondrej Ezr <oezr@redhat.com>
2022-07-18 13:55:20 +0200

    Add Account DAO stubs

---
+++ internal/dao/stubs/context_getters.go
@@ -0,0 +1,42 @@
+package stubs
+
+import (
+	"context"
+
+	"github.com/RHEnVision/provisioning-backend/internal/models"
+)
+
+type daoStubCtxKeyType int
+
+const (
+	accountCtxKey daoStubCtxKeyType = iota
+	pubkeyCtxKey  daoStubCtxKeyType = iota
+)
+
+func WithPubkeyDao(parent context.Context, init_store []*models.Pubkey) context.Context {
+	ctx := context.WithValue(parent, pubkeyCtxKey, &PubkeyDaoStub{init_store})
+	return ctx
+}
+
+func getPubkeyDaoStub(ctx context.Context) (*PubkeyDaoStub, error) {
+	var ok bool
+	var pkdao *PubkeyDaoStub
+	if pkdao, ok = ctx.Value(pubkeyCtxKey).(*PubkeyDaoStub); !ok {
+		return nil, ContextReadError
+	}
+	return pkdao, nil
+}
+
+func WithAccountDaoOne(parent context.Context) context.Context {
+	ctx := context.WithValue(parent, accountCtxKey, buildAccountDaoWithOneAccount())
+	return ctx
+}
+
+func getAccountDaoStub(ctx context.Context) (*accountDaoStub, error) {
+	var ok bool
+	var accdao *accountDaoStub
+	if accdao, ok = ctx.Value(accountCtxKey).(*accountDaoStub); !ok {
+		return nil, ContextReadError
+	}
+	return accdao, nil
+}
--- internal/dao/stubs/pubkey_dao.go
+++ internal/dao/stubs/pubkey_dao.go
@@ -7,27 +7,12 @@ import (
 	"github.com/RHEnVision/provisioning-backend/internal/models"
 )
 
-type pubkeyCtxKeyType string
-
-var pubkeyCtxKey pubkeyCtxKeyType = "pubkey-dao"
-
 type PubkeyDaoStub struct {
 	store []*models.Pubkey
 }
 
 func init() {
 	dao.GetPubkeyDao = getPubkeyDao
-}
-
-type contextReadError struct{}
-
-func (m *contextReadError) Error() string {
-	return "failed to find or convert dao stored in testing context"
-}
-
-func WithPubkeyDao(parent context.Context, init_store []*models.Pubkey) context.Context {
-	ctx := context.WithValue(parent, pubkeyCtxKey, &PubkeyDaoStub{init_store})
-	return ctx
 }
 
 func PubkeyStubCount(ctx context.Context) (int, error) {
@@ -40,14 +25,6 @@ func PubkeyStubCount(ctx context.Context) (int, error) {
 
 func getPubkeyDao(ctx context.Context) (dao.PubkeyDao, error) {
 	return getPubkeyDaoStub(ctx)
-}
-
-func getPubkeyDaoStub(ctx context.Context) (pkdao *PubkeyDaoStub, err error) {
-	var ok bool
-	if pkdao, ok = ctx.Value(pubkeyCtxKey).(*PubkeyDaoStub); !ok {
-		err = &contextReadError{}
-	}
-	return pkdao, err
 }
 
 func (mock *PubkeyDaoStub) Create(ctx context.Context, pk *models.Pubkey) error {
