52854181ced0e78e279b3b11799e27d9bc648761
Ondrej Ezr <oezr@redhat.com>
2022-07-20 20:22:35 +0200

    Implement all pubkey DAO functions
    Implement all pubkey DAO functions in the stub.
    This prepares ground for testing everything we want from the real implementation.

--- internal/dao/stubs/pubkey_dao.go
+++ internal/dao/stubs/pubkey_dao.go
@@ -28,17 +28,42 @@ func getPubkeyDao(ctx context.Context) (dao.PubkeyDao, error) {
 	return getPubkeyDaoStub(ctx)
 }
 
-func (mock *PubkeyDaoStub) Create(ctx context.Context, pk *models.Pubkey) error {
-	pk.ID = mock.lastId + 1
-	mock.store = append(mock.store, pk)
-	mock.lastId++
+func (stub *PubkeyDaoStub) Create(ctx context.Context, pk *models.Pubkey) error {
+	pk.ID = stub.lastId + 1
+	stub.store = append(stub.store, pk)
+	stub.lastId++
 	return nil
 }
-func (*PubkeyDaoStub) Update(ctx context.Context, pk *models.Pubkey) error { return nil }
-func (mock *PubkeyDaoStub) GetById(ctx context.Context, id int64) (*models.Pubkey, error) {
-	return mock.store[0], nil
+
+func (stub *PubkeyDaoStub) Update(ctx context.Context, pk *models.Pubkey) error {
+	for idx, p := range stub.store {
+		if p.ID == pk.ID {
+			stub.store[idx] = pk
+			return nil
+		}
+	}
+	return NewRecordNotFoundError(ctx, "Pubkey")
 }
-func (mock *PubkeyDaoStub) List(ctx context.Context, limit, offset int64) ([]*models.Pubkey, error) {
-	return mock.store, nil
+
+func (stub *PubkeyDaoStub) GetById(ctx context.Context, id int64) (*models.Pubkey, error) {
+	for _, acc := range stub.store {
+		if acc.ID == id {
+			return acc, nil
+		}
+	}
+	return nil, NewRecordNotFoundError(ctx, "Pubkey")
 }
-func (*PubkeyDaoStub) Delete(ctx context.Context, id int64) error { return nil }
+
+func (stub *PubkeyDaoStub) List(ctx context.Context, limit, offset int64) ([]*models.Pubkey, error) {
+	return stub.store, nil
+}
+
+func (stub *PubkeyDaoStub) Delete(ctx context.Context, id int64) error {
+	for idx, p := range stub.store {
+		if p.ID == id {
+			stub.store = append(stub.store[:idx], stub.store[idx+1:]...)
+			return nil
+		}
+	}
+	return nil
+}
