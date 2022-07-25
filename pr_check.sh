c2e9cc3f0be935f5a34b871870330ebef2e2997c
Mike Shriver <mshriver@redhat.com>
2022-07-25 09:24:31 -0400

    Update makefile, add pr_checks and build_deploy
    These will be used by appsre CI for building the main branch image and
    pushing to quay.io/cloudservices, and for executing PR checks against
    all PRs opened in this repo.
    
    pr_checks will call CI scripts from a shared repo to build an image with
    the PR content and run iqe tests against it
    
    build_deploy will execute against the main branch after PR merges.
    
    update clowdapp to use quay.io/cloudservices image by default

---
+++ pr_check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+APP_NAME="provisioning"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="provisioning-backend"  # name of resourceTemplate component for deploy
+IMAGE="quay.io/cloudservices/provisioning-backend"  # image location on quay
+DOCKERFILE="build/Dockerfile"
+
+IQE_PLUGINS="hms-provisioning"  # name of the IQE plugin for this app.
+IQE_MARKER_EXPRESSION=""  # This is the value passed to pytest -m
+IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
+IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to complete or fail
+
+
+# Install bonfire repo/initialize
+# https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd/bootstrap.sh
+# This script automates the install / config of bonfire
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# This script is used to build the image that is used in the PR Check
+source $CICD_ROOT/build.sh
+
+# This script is used to deploy the ephemeral environment for smoke tests.
+# The manual steps for this can be found in:
+# https://internal.cloud.redhat.com/docs/devprod/ephemeral/02-deploying/
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+# Run smoke tests using a ClowdJobInvocation and iqe-tests
+COMPONENT_NAME="provisioning"  # name of app used in deploy-iqe-cji
+source $CICD_ROOT/cji_smoke_test.sh
+
+# Post a comment with test run IDs to the PR
+# source $CICD_ROOT/post_test_results.sh
