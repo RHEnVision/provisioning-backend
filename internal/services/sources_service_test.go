a00ce1af015a78b07b3435992a2a4df1cfb8091b
Ondrej Ezr <oezr@redhat.com>
2022-07-27 12:10:53 +0200

    Clean up unused sources test fixtures

--- internal/services/sources_service_test.go
+++ internal/services/sources_service_test.go
@@ -11,27 +11,8 @@ import (
 	sources "github.com/RHEnVision/provisioning-backend/internal/clients/sources"
 	"github.com/RHEnVision/provisioning-backend/internal/clients/sources/stubs"
 	"github.com/RHEnVision/provisioning-backend/internal/testing/identity"
-	"github.com/aws/smithy-go/ptr"
 	"github.com/stretchr/testify/assert"
 )
-
-func buildSourcesStore() *[]sources.Source {
-	var TestSourceData = []sources.Source{
-		{
-			Id:           ptr.String("1"),
-			Name:         ptr.String("source1"),
-			SourceTypeId: ptr.String("1"),
-			Uid:          ptr.String("5eebe172-7baa-4280-823f-19e597d091e9"),
-		},
-		{
-			Id:           ptr.String("2"),
-			Name:         ptr.String("source2"),
-			SourceTypeId: ptr.String("2"),
-			Uid:          ptr.String("31b5338b-685d-4056-ba39-d00b4d7f19cc"),
-		},
-	}
-	return &TestSourceData
-}
 
 func TestListSourcesHandler(t *testing.T) {
 	t.SkipNow()
