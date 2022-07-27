dca008233594b0a7d5980807e552fd6c55052f85
Ondrej Ezr <oezr@redhat.com>
2022-07-27 12:08:53 +0200

    Clean up old sources client approach

--- internal/services/sources_service.go
+++ internal/services/sources_service.go
@@ -1,15 +1,10 @@
 package services
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
 	sources "github.com/RHEnVision/provisioning-backend/internal/clients/sources"
-	"github.com/RHEnVision/provisioning-backend/internal/headers"
-	"github.com/RHEnVision/provisioning-backend/internal/parsing"
 	"github.com/RHEnVision/provisioning-backend/internal/payloads"
-	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 )
 
@@ -29,94 +24,4 @@ func ListSources(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-}
-
-//TODO: to remove after changing to new sources client
-func GetSource(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "ID")
-
-	client, err := sources.GetSourcesClient(r.Context())
-	if err != nil {
-		renderError(w, r, payloads.NewClientInitializationError(r.Context(), "sources client", err))
-		return
-	}
-
-	resp, err := client.ShowSourceWithResponse(r.Context(), sources.ID(id), headers.AddIdentityHeader)
-	statusCode := resp.StatusCode()
-	if err != nil {
-		renderError(w, r, payloads.SourcesClientError(r.Context(), "show source", err, statusCode))
-		return
-	}
-	if parsing.IsHTTPNotFound(statusCode) {
-		renderError(w, r, payloads.SourcesClientError(r.Context(), "source not found", err, statusCode))
-		return
-	}
-	if !parsing.IsHTTPStatus2xx(statusCode) {
-		renderError(w, r, payloads.SourcesClientError(r.Context(), "sources client error", err, statusCode))
-		return
-	}
-	if err := render.Render(w, r, payloads.NewShowSourcesResponse(resp.JSON200)); err != nil {
-		renderError(w, r, payloads.NewRenderError(r.Context(), "show source", err))
-	}
-}
-
-//TODO: to remove after changing to new sources client
-func fetchARN(ctx context.Context, client sources.SourcesIntegration, sourceId string) (string, error) {
-	// Get all the authentications linked to a specific source
-	resp, err := client.ListSourceAuthenticationsWithResponse(ctx, sourceId, &sources.ListSourceAuthenticationsParams{}, headers.AddIdentityHeader)
-	if err != nil {
-		return "", fmt.Errorf("cannot list source authentication: %w", err)
-	}
-	statusCode := resp.StatusCode()
-	if parsing.IsHTTPNotFound(statusCode) {
-		return "", sources.AuthenticationForSourcesNotFoundErr
-	}
-	if !parsing.IsHTTPStatus2xx(statusCode) {
-		return "", sources.SourcesClientErr
-	}
-	// Filter authentications to include only auth where resource_type == "Application"
-	auth, err := filterSourceAuthentications(resp.JSON200.Data)
-	if err != nil {
-		return "", err
-	}
-	// Get the resource_id which equals to application_id
-	// and check that application_type_id in /applications/<app_id> equals to provisioning id
-	res, err := client.ShowApplicationWithResponse(ctx, *auth.ResourceId, headers.AddIdentityHeader)
-	if err != nil {
-		return "", fmt.Errorf("cannot list source authentication: %w", err)
-	}
-	statusCode = res.StatusCode()
-	if parsing.IsHTTPNotFound(statusCode) {
-		return "", sources.ApplicationNotFoundErr
-	}
-	if !parsing.IsHTTPStatus2xx(statusCode) {
-		return "", sources.SourcesClientErr
-	}
-
-	appTypeId, err := client.GetProvisioningTypeId(ctx, headers.AddIdentityHeader)
-	if err != nil {
-		return "", fmt.Errorf("cannot get provisioning app type: %w", err)
-	}
-
-	if *res.JSON200.ApplicationTypeId == appTypeId {
-		return *auth.Username, nil
-
-	}
-	return "", fmt.Errorf("cannot find authentication linked to source id %s and to the provisioning app: %w", sourceId, err)
-}
-
-//TODO: to remove after changing to new sources client
-func filterSourceAuthentications(authentications *[]sources.AuthenticationRead) (sources.AuthenticationRead, error) {
-	auths := *authentications
-	list := make([]sources.AuthenticationRead, 0, len(auths))
-	for _, auth := range auths {
-		if *auth.ResourceType == "Application" {
-			list = append(list, auth)
-		}
-	}
-	// Assumption: each source has one authentication linked to it
-	if len(list) > 1 {
-		return sources.AuthenticationRead{}, sources.MoreThenOneAuthenticationForSourceErr
-	}
-	return list[0], nil
 }
