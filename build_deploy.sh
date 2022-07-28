7875c895234899a5a1d2255130f2fe10a0524837
Mike Shriver <mshriver@redhat.com>
2022-07-28 13:16:27 -0400

    HMSPROV-115: Update makefile, add pr_checks and build_deploy (#110)
    * Update makefile, add pr_checks and build_deploy
    
    These will be used by appsre CI for building the main branch image and
    pushing to quay.io/cloudservices, and for executing PR checks against
    all PRs opened in this repo.
    
    pr_checks will call CI scripts from a shared repo to build an image with
    the PR content and run iqe tests against it
    
    build_deploy will execute against the main branch after PR merges.
    
    update clowdapp to use quay.io/cloudservices image by default
    
    * Update clowdapp name
    
    It has to match the resourceTemplate component name

---
+++ build_deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+IMAGE="${IMAGE:-quay.io/cloudservices/provisioning-backend}"
+IMAGE_TAG="$(git rev-parse --short=7 HEAD)"
+SMOKE_TEST_TAG="latest"
+
+if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+    echo "QUAY_USER and QUAY_TOKEN must be set"
+    exit 1
+fi
+
+if [[ -z "$RH_REGISTRY_USER" || -z "$RH_REGISTRY_TOKEN" ]]; then
+    echo "RH_REGISTRY_USER and RH_REGISTRY_TOKEN  must be set"
+    exit 1
+fi
+
+# Login to quay
+podman login \
+    -u ${QUAY_USER} \
+    -p ${QUAY_TOKEN} \
+    quay.io
+
+# Login to registry.redhat
+podman login \
+    -u ${RH_REGISTRY_USER} \
+    -p ${RH_REGISTRY_TOKEN} \
+    registry.redhat.io
+
+# build and push
+make build-podman \
+    CONTAINER_IMAGE="${IMAGE}:${IMAGE_TAG}"
+
+# push to logged in registries and tag for SHA
+podman tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:${SMOKE_TEST_TAG}"
+podman push "${IMAGE}:${SMOKE_TEST_TAG}"
