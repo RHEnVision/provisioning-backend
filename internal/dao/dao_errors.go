7fe59777c87e24c95315db51c28515eb0be520a5
Ondřej Ezr <oezr@redhat.com>
2022-08-01 09:13:28 +0200

    HMSPROV-158: Add validations to pubkeys (#106)
    * HMSPROV-158: Add validations to pubkeys
    
    Adds general validations for models.
    Adds pubkeys validations on Body and Name.
    
    * Stubs to implement NameForError
    
    Use the same concepts for errors in real and stubbed DAOs to share errors.

--- internal/dao/dao_errors.go
+++ internal/dao/dao_errors.go
@@ -4,13 +4,30 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/RHEnVision/provisioning-backend/internal/ctxval"
+	"github.com/go-playground/validator/v10"
 )
+
+type NamedForError interface {
+	// NameForError returns DAO implementation name that is passed in the error message (e.g. "account").
+	NameForError() string
+}
 
 // Error represents a common DAO error.
 type Error struct {
 	Message string
 	Context context.Context
 	Err     error
+}
+
+// ValidationError is returned when validation on model fails
+type ValidationError struct {
+	Message string
+	Context context.Context
+	Err     error
+	Model   interface{}
 }
 
 // NoRowsError is returned when no rows were returned.
@@ -36,6 +53,14 @@ func (e Error) Unwrap() error {
 	return e.Err
 }
 
+func (e ValidationError) Error() string {
+	return fmt.Sprintf("DAO error: %s: %s", e.Message, e.Err.Error())
+}
+
+func (e ValidationError) Unwrap() error {
+	return e.Err
+}
+
 func (e NoRowsError) Error() string {
 	return fmt.Sprintf("DAO no rows returned: %s", e.Message)
 }
@@ -46,4 +71,17 @@ func (e NoRowsError) Unwrap() error {
 
 func (e MismatchAffectedError) Error() string {
 	return fmt.Sprintf("DAO mismatch affected rows: %s", e.Message)
+}
+
+func NewValidationError(ctx context.Context, dao NamedForError, model interface{}, validationErr validator.ValidationErrors) ValidationError {
+	errors := []string{fmt.Sprintf("Validation of %s failed: ", dao.NameForError())}
+	for _, ve := range validationErr {
+		errors = append(errors, ve.Error())
+	}
+	msg := strings.Join(errors, ", ")
+
+	if logger := ctxval.Logger(ctx); logger != nil {
+		logger.Info().Msg(msg)
+	}
+	return ValidationError{Context: ctx, Message: msg, Err: validationErr, Model: model}
 }
