6e8ff19fe683623c2d098333f1fa5d559644778e
Ondřej Ezr <oezr@redhat.com>
2022-08-02 11:26:11 +0200

    Clean up unused methods from Sources API client (#133)
    Rename client to the API client and clean up methods that were move

--- internal/clients/sources/sources_interface.go
+++ internal/clients/sources/sources_interface.go
@@ -7,7 +7,6 @@ import (
 var GetSourcesAPIClient func(ctx context.Context) (APIClient, error)
 
 type APIClient interface {
-	GetProvisioningTypeId(ctx context.Context, reqEditors ...RequestEditorFn) (string, error)
 	ShowSourceWithResponse(ctx context.Context, id ID, reqEditors ...RequestEditorFn) (*ShowSourceResponse, error)
 	ListApplicationTypeSourcesWithResponse(ctx context.Context, appTypeId ID, params *ListApplicationTypeSourcesParams, reqEditors ...RequestEditorFn) (*ListApplicationTypeSourcesResponse, error)
 	ListSourceAuthenticationsWithResponse(ctx context.Context, id ID, params *ListSourceAuthenticationsParams, reqEditors ...RequestEditorFn) (*ListSourceAuthenticationsResponse, error)
