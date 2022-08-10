56864dcefa741993fe7c5e6dc699b0202c5be762
Lukas Zapletal <lzap+git@redhat.com>
2022-08-10 12:59:52 +0200

    Implement setup/teardown methods in idiomatic Go

---
+++ cmd/dao_tests/main_test.go
@@ -0,0 +1,65 @@
+//go:build integration
+// +build integration
+
+// To override application configuration for integration tests, copy local.yaml into this directory.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/RHEnVision/provisioning-backend/internal/config"
+	"github.com/RHEnVision/provisioning-backend/internal/db"
+	"github.com/RHEnVision/provisioning-backend/internal/logging"
+	"github.com/rs/zerolog/log"
+)
+
+func setup() {
+	dbDrop()
+	dbMigrate()
+	dbSeed()
+}
+
+func teardown() {
+	// nothing
+}
+
+func initEnvironment() {
+	config.Initialize()
+	log.Logger = logging.InitializeStdout()
+
+	err := db.Initialize("integration")
+	if err != nil {
+		panic(fmt.Errorf("cannot connect to database, create cmd/dao_tests/local.yaml: %v", err))
+	}
+}
+
+func dbDrop() {
+	err := db.Seed("drop_integration")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func dbMigrate() {
+	err := db.Migrate("integration")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func dbSeed() {
+	err := db.Seed("dao_test")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(t *testing.M) {
+	initEnvironment()
+	exitVal := t.Run()
+	dbDrop()
+	os.Exit(exitVal)
+}
