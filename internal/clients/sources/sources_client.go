36feec90a6513dbf4ece64afad786e85f4102ed1
Amir Fefer <amirfefer@gmail.com>
2022-08-22 19:36:00 +0300

    add identity header to ListApplicationTypes

--- internal/clients/sources/sources_client.go
+++ internal/clients/sources/sources_client.go
@@ -164,7 +164,7 @@ func (c *SourcesClient) GetProvisioningTypeId(ctx context.Context) (string, erro
 
 func (c *SourcesClient) loadAppId(ctx context.Context) (string, error) {
 	ctxval.Logger(ctx).Info().Msg("Fetching the Application Type ID of Provisioning for Sources")
-	resp, err := c.client.ListApplicationTypes(ctx, &ListApplicationTypesParams{})
+	resp, err := c.client.ListApplicationTypes(ctx, &ListApplicationTypesParams{}, headers.AddSourcesIdentityHeader)
 	if err != nil {
 		ctxval.Logger(ctx).Warn().Err(err).Msg("Failed to fetch ApplicationTypes from sources")
 		return "", fmt.Errorf("failed to fetch ApplicationTypes: %w", err)
