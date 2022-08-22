baed774be2e8a9a84af046a95b1b6d8012f63771
Ondrej Ezr <oezr@redhat.com>
2022-08-22 14:14:45 +0200

    Drop account endpoints
    We might want to add this at some point for better observability, but we 
    need to figure out admin/service access gating first, which is not yet 
    on the table.

--- internal/routes/all_routes.go
+++ internal/routes/all_routes.go
@@ -47,13 +47,6 @@ func apiRouter() http.Handler {
 			r.Get("/", s.ListSources)
 		})
 
-		r.Route("/accounts", func(r chi.Router) {
-			r.Get("/", s.ListAccounts)
-			r.Route("/{ID}", func(r chi.Router) {
-				r.Get("/", s.GetAccount)
-			})
-		})
-
 		r.Route("/pubkeys", func(r chi.Router) {
 			r.Post("/", s.CreatePubkey)
 			r.Get("/", s.ListPubkeys)
