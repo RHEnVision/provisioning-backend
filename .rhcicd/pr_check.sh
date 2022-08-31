11a6ad52eb26047a43204303eff61cbee4ac5e13
Mayuri Hadole <mlahane@mlahane.pnq.csb>
2022-08-31 14:07:22 -0400

    Changed plugin name

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -8,7 +8,7 @@ COMPONENT_NAME="provisioning-backend"  # name of resourceTemplate component for 
 IMAGE="quay.io/cloudservices/provisioning-backend"  # image location on quay
 DOCKERFILE="build/Dockerfile"
 
-IQE_PLUGINS="hms-provisioning"  # name of the IQE plugin for this app.
+IQE_PLUGINS="provisioning"  # name of the IQE plugin for this app.
 IQE_MARKER_EXPRESSION=""  # This is the value passed to pytest -m
 IQE_FILTER_EXPRESSION=""  # This is the value passed to pytest -k
 IQE_CJI_TIMEOUT="30m"  # This is the time to wait for smoke test to complete or fail
