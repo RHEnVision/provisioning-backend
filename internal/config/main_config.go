8f1132ec24f3c7658023907515f24f814ca973d5
Lukas Zapletal <lzap+git@redhat.com>
2022-09-02 16:51:17 +0200

    Move dao tests into internal/ and fix config
    Previously we thought that dao_tests will be a binary, but we ended up using unit tests. This means we can now move them into the proper folder.
    
    ACTION REQUIRED: This patch also fixes configuration loading, it now properly finds the path to the config file. Please move your `cmd/dao_test/local.yaml` to `configs/local.integration.yaml` to get it loaded properly.
    
    I encountered different behavior when running integration tests via `go test` utility (which changes working directory to the package) and IDE (GoLand) which does not. For this reason, the file is searched in both these locations.

--- internal/config/main_config.go
+++ internal/config/main_config.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/RHEnVision/provisioning-backend/internal/config/parser"
+	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
 	"github.com/rs/zerolog"
 )
 
@@ -100,6 +101,21 @@ var Sources = &config.RestEndpoints.Sources
 var Worker = &config.Worker
 
 func Initialize() {
+	if clowder.IsClowderEnabled() {
+		cfg := clowder.LoadedConfig
+		parser.Viper.Set("featureFlags.environment", "production")
+		parser.Viper.Set("database.host", cfg.Database.Hostname)
+		parser.Viper.Set("database.port", cfg.Database.Port)
+		parser.Viper.Set("database.user", cfg.Database.Username)
+		parser.Viper.Set("database.password", cfg.Database.Password)
+		parser.Viper.Set("database.name", cfg.Database.Name)
+		parser.Viper.Set("prometheus.port", cfg.MetricsPort)
+		parser.Viper.Set("prometheus.path", cfg.MetricsPath)
+		if endpoint, ok := clowder.DependencyEndpoints["sources-api"]["svc"]; ok {
+			parser.Viper.Set("restEndpoints.sources.url", fmt.Sprintf("http://%s:%d/api/sources/v3.1", endpoint.Hostname, endpoint.Port))
+		}
+	}
+
 	err := parser.Viper.Unmarshal(&config)
 	if err != nil {
 		panic(err)
