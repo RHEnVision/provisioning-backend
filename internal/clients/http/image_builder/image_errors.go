373c35cb36da3aa8e5214715968a22b9f40b6095
Lukas Zapletal <lzap+git@redhat.com>
2022-09-09 12:03:54 +0200

    Improve NotFound errors

--- internal/clients/http/image_builder/image_errors.go
+++ internal/clients/http/image_builder/image_errors.go
@@ -2,10 +2,12 @@ package image_builder
 
 import (
 	"errors"
+	"fmt"
+
+	"github.com/RHEnVision/provisioning-backend/internal/clients"
 )
 
-var ClientErr = errors.New("image builder client error")
-var ComposeNotFoundErr = errors.New("image was not not found")
+var ComposeNotFoundErr = fmt.Errorf("image compose not found: %w", clients.NotFoundErr)
 var ImageStatusErr = errors.New("build of requested image has not finished yet")
 var UnknownImageTypeErr = errors.New("unknown image type")
 var AmiNotFoundInStatusErr = errors.New("AMI not found in image status")
