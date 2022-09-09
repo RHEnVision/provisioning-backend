8cd39dc2d07ce3649829b9ffc8e96a3402dd184c
Lukas Zapletal <lzap+git@redhat.com>
2022-09-09 10:00:41 +0200

    Improve error handling of sources client

--- internal/clients/clients_errors.go
+++ internal/clients/clients_errors.go
@@ -2,5 +2,11 @@ package clients
 
 import "errors"
 
-var UnauthorizedErr = errors.New("operation not permitted by client, check the provided account permissions")
+// TODO rename to *Err and 2xx!!
+var NotFoundError = errors.New("backend service returned not found (404)")
+var UnauthorizedErr = errors.New("backend service returned unauthorized (401)")
+var ForbiddenErr = errors.New("backend service returned forbidden (403)")
+var Non200ResponseErr = errors.New("backend service did not return 2xx")
+
+// TODO this should not be here, it's AWS specific
 var DuplicatePubkeyErr = errors.New("pubkey already exists in the target account")
--- internal/parsing/parsing.go
+++
@@ -1,9 +0,0 @@
-package parsing
-
-func IsHTTPStatus2xx(status int) bool {
-	return status >= 200 && status <= 299
-}
-
-func IsHTTPNotFound(status int) bool {
-	return status == 404
-}
