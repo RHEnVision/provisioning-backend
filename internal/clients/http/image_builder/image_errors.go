753877f73cc228d10d19523ae866d5daf2c13de2
Lukas Zapletal <lzap+git@redhat.com>
2022-09-09 10:33:22 +0200

    Improve error handling of image builder client

--- internal/clients/http/image_builder/image_errors.go
+++ internal/clients/http/image_builder/image_errors.go
@@ -7,4 +7,5 @@ import (
 var ClientErr = errors.New("image builder client error")
 var ComposeNotFoundErr = errors.New("image was not not found")
 var ImageStatusErr = errors.New("build of requested image has not finished yet")
-var BadImageTypeErr = errors.New("image type is wrong and can not be used for target platform")
+var UnknownImageTypeErr = errors.New("unknown image type")
+var AmiNotFoundInStatusErr = errors.New("AMI not found in image status")
