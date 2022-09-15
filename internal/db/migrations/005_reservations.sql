1f35a7432addddb0b881efa672106a19099ad591
aabramov <aabramov@redhat.com>
2022-09-15 11:09:53 +0300

    Add new migration file for gcp

--- internal/db/migrations/005_reservations.sql
+++ internal/db/migrations/005_reservations.sql
@@ -33,17 +33,3 @@ CREATE TABLE aws_reservation_details
   FOREIGN KEY (reservation_id, provider) REFERENCES reservations(id, provider) ON DELETE CASCADE,
   CHECK (provider = provider_type_aws())
 );
-
-CREATE TABLE gcp_reservation_details
-(
-  reservation_id BIGINT NOT NULL,
-  provider INTEGER NOT NULL DEFAULT provider_type_gcp(),
-  pubkey_id BIGINT NOT NULL REFERENCES pubkeys(id),
-  source_id TEXT NOT NULL,
-  image_id TEXT NOT NULL,
-  gcp_reservation_id TEXT,
-  detail JSONB,
-
-  FOREIGN KEY (reservation_id, provider) REFERENCES reservations(id, provider) ON DELETE CASCADE,
-  CHECK (provider = provider_type_gcp())
-);
---
+++ internal/db/migrations/007_gcp_details.sql
@@ -0,0 +1,1 @@
+ALTER FUNCTION provider_type_gce () RENAME TO provider_type_gcp;
