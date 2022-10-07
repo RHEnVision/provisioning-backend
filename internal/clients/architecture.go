13325c5386a2f8b1802ed4f777fe6dd833fcf7ba
Anna Vitova <avitova@redhat.com>
2022-10-07 09:58:25 +0200

    Rename architectures X86_64

--- internal/clients/architecture.go
+++ internal/clients/architecture.go
@@ -10,12 +10,11 @@ import (
 type ArchitectureType string
 
 const (
-	ArchitectureTypeI386 ArchitectureType = "i386"
-	// TODO rename to X86_64
-	ArchitectureTypeX8664      ArchitectureType = "x86_64"
-	ArchitectureTypeArm64      ArchitectureType = "arm64"
-	ArchitectureTypeAppleX8664 ArchitectureType = "apple-x86_64"
-	ArchitectureTypeAppleArm64 ArchitectureType = "apple-arm64"
+	ArchitectureTypeI386        ArchitectureType = "i386"
+	ArchitectureTypeX86_64      ArchitectureType = "x86_64"
+	ArchitectureTypeArm64       ArchitectureType = "arm64"
+	ArchitectureTypeAppleX86_64 ArchitectureType = "apple-x86_64"
+	ArchitectureTypeAppleArm64  ArchitectureType = "apple-arm64"
 )
 
 func (at *ArchitectureType) String() string {
@@ -25,13 +24,13 @@ func (at *ArchitectureType) String() string {
 func MapArchitectures(_ context.Context, arch string) (ArchitectureType, error) {
 	switch {
 	case arch == "x86_64_mac":
-		return ArchitectureTypeAppleX8664, nil
+		return ArchitectureTypeAppleX86_64, nil
 	case arch == "arm64_mac":
 		return ArchitectureTypeAppleArm64, nil
 	case arch == "i386":
 		return ArchitectureTypeI386, nil
 	case arch == "x86-64" || arch == "x86_64" || arch == "x64":
-		return ArchitectureTypeX8664, nil
+		return ArchitectureTypeX86_64, nil
 	case arch == "aarch64" || arch == "arm64" || arch == "Arm64" || arch == "arm":
 		return ArchitectureTypeArm64, nil
 	}
