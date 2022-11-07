0d0e39441b515f649d7f0e6b8259e0019c0e3ef4
Lukas Zapletal <lzap+git@redhat.com>
2022-11-07 17:07:34 +0100

    chore: update all deps

--- mk/deps.mk
+++ mk/deps.mk
@@ -11,6 +11,8 @@ download-deps: ## Download Go modules
 .PHONY: update-deps
 update-deps: ## Update Go modules to latest versions
 	go get -u all
+	go get github.com/jackc/puddle/v2@v2.0.0 # https://github.com/jackc/puddle/issues/26
+	go get github.com/jackc/tern/v2@v2.0.0-beta.3 # not released yet
 	go mod tidy
 
 # alias for download-deps
