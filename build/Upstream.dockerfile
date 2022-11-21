71c208a3e7d1911e3049fc04371055a42e489841
Lukas Zapletal <lzap+git@redhat.com>
2022-11-21 09:00:25 +0100

    feat: statuser clowder deployment

--- build/Upstream.dockerfile
+++ build/Upstream.dockerfile
@@ -15,6 +15,7 @@ RUN make prep build strip
 FROM registry.access.redhat.com/ubi8/ubi-minimal
 COPY --from=build /build/pbapi /pbapi
 COPY --from=build /build/pbworker /pbworker
+COPY --from=build /build/pbstatuser /pbstatuser
 COPY --from=build /build/pbmigrate /pbmigrate
 USER 1001
 CMD ["/pbapi"]
--- mk/compile.mk
+++ mk/compile.mk
@@ -7,15 +7,20 @@ SRC_YAML := $(shell find . -name \*.yaml -print)
 PACKAGE_BASE = github.com/RHEnVision/provisioning-backend/internal
 LDFLAGS = "-X $(PACKAGE_BASE)/version.BuildCommit=$(shell git rev-parse --short HEAD) -X $(PACKAGE_BASE)/version.BuildTime=$(shell date +'%Y-%m-%d_%T')"
 
-build: pbapi pbmigrate pbworker ## Build all binaries
+build: pbapi pbmigrate pbworker pbstatuser ## Build all binaries
 
-pbapi: $(SRC_GO) $(SRC_SQL) $(SRC_YAML) ## Build backend API service
+all-deps: $(SRC_GO) $(SRC_SQL) $(SRC_YAML)
+
+pbapi: all-deps ## Build backend API service
 	CGO_ENABLED=0 go build -ldflags $(LDFLAGS) -o pbapi ./cmd/pbapi
 
-pbworker: $(SRC_GO) $(SRC_SQL) $(SRC_YAML) ## Build worker service
+pbworker: all-deps ## Build worker service
 	CGO_ENABLED=0 go build -ldflags $(LDFLAGS) -o pbworker ./cmd/pbworker
 
-pbmigrate: $(SRC_GO) $(SRC_SQL) $(SRC_YAML) ## Build migration command
+pbstatuser: all-deps ## Build status worker command
+	CGO_ENABLED=0 go build -o pbstatuser ./cmd/pbstatuser
+
+pbmigrate: all-deps ## Build migration command
 	CGO_ENABLED=0 go build -o pbmigrate ./cmd/pbmigrate
 
 .PHONY: strip
