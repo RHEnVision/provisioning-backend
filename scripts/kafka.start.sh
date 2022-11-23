2787d12b4087c689aad56f020169f8b8162d15eb
Lukas Zapletal <lzap+git@redhat.com>
2022-11-23 08:10:11 +0100

    fix: create topics in kafka startup script

--- scripts/kafka.start.sh
+++ scripts/kafka.start.sh
@@ -8,3 +8,6 @@ $BASEDIR/kafka/bin/kafka-storage.sh format -t $UUID -c $BASEDIR/kafka/config/kra
 
 echo "Starting Kafka..."
 $BASEDIR/kafka/bin/kafka-server-start.sh $BASEDIR/kafka/config/kraft/server.properties
+
+echo "Creating topics..."
+$BASEDIR/kafka/bin/kafka-topics.sh --create --topic platform.provisioning.internal.availability-check --bootstrap-server $KAFKA_HOST
