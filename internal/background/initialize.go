336b1df158dc932a26c4ceca2d601f56df9f1892
Lukas Zapletal <lzap+git@redhat.com>
2023-02-15 14:21:51 +0100

    fix(HMS-860): modify and update job queue metrics

--- internal/background/initialize.go
+++ internal/background/initialize.go
@@ -13,11 +13,22 @@ import (
 // incoming requests to overload the sender.
 const availabilityStatusBatchSize = 1024
 
-// Initialize starts background goroutines. Use context cancellation to stop them.
-func Initialize(ctx context.Context) {
+// InitializeApi starts background goroutines for REST API processes.
+// Use context cancellation to stop them.
+func InitializeApi(ctx context.Context) {
 	logger := ctxval.Logger(ctx).With().Bool("background", true).Logger()
 	ctx = ctxval.WithLogger(ctx, &logger)
 
 	// start availability request batch sender
 	go sendAvailabilityRequestMessages(ctx, availabilityStatusBatchSize, 5*time.Second)
+}
+
+// InitializeWorker starts background goroutines for worker processes.
+// Use context cancellation to stop them.
+func InitializeWorker(ctx context.Context, workerName string) {
+	logger := ctxval.Logger(ctx).With().Bool("background", true).Logger()
+	ctx = ctxval.WithLogger(ctx, &logger)
+
+	// start job queue telemetry
+	go jobQueueMetricLoop(ctx, 30*time.Second, workerName)
 }
---
+++ internal/background/queue_metrics.go
@@ -0,0 +1,40 @@
+package background
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	"github.com/RHEnVision/provisioning-backend/internal/ctxval"
+	"github.com/RHEnVision/provisioning-backend/internal/metrics"
+	"github.com/RHEnVision/provisioning-backend/internal/queue/jq"
+)
+
+//nolint:gosec
+// jobQueueMetricLoop is a background function that runs for all workers.
+// It polls job queue statistics from Redis as well as in-flight counters.
+// It waits a random delay between 0 and sleep for better polling spread
+// since job queue size is a global metric (redis queue length).
+func jobQueueMetricLoop(ctx context.Context, sleep time.Duration, name string) {
+	logger := ctxval.Logger(ctx)
+
+	// spread polling intervals
+	randSleep := rand.Int63() % sleep.Milliseconds()
+	logger.Debug().Msgf("Job queue metric delay %dms", randSleep)
+	time.Sleep(time.Duration(randSleep) * time.Millisecond)
+	ticker := time.NewTicker(sleep)
+
+	for {
+		select {
+		case <-ticker.C:
+			stats := jq.Stats(ctx)
+			metrics.SetJobQueueSize(stats.EnqueuedJobs)
+			metrics.SetJobQueueInFlight(name, stats.InFlight)
+
+		case <-ctx.Done():
+			ticker.Stop()
+			logger.Debug().Msg("Stopping job queue metric loop")
+			return
+		}
+	}
+}
