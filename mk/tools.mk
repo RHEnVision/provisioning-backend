9d5aefa8c5a59ced5aa59c41c8b8db12b7a5d34b
Anna Vitova <avitova@redhat.com>
2023-03-08 09:39:45 +0100

    chore: Update Makefile to work for Go 1.18

--- mk/tools.mk
+++ mk/tools.mk
@@ -3,7 +3,7 @@
 .PHONY: install-tools
 install-tools: ## Install required Go commands
 	go install golang.org/x/tools/cmd/goimports@latest
-	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+	go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1
 	go install github.com/jackc/tern@latest
 	go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest
 	go install mvdan.cc/gofumpt@latest
