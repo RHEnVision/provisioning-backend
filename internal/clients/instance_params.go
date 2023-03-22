5e9a849dde52f962148e1045ff9e072a264c457f
aabramov <aabramov@redhat.com>
2023-03-22 15:49:45 +0200

    refactor: Create struct for create instances requests

---
+++ internal/clients/instance_params.go
@@ -0,0 +1,40 @@
+package clients
+
+import "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
+type GCPInstanceParams struct {
+	// The string pattern used for the name of the VM.
+	NamePattern *string
+
+	// Image Name the instance will be launched from
+	ImageName string
+
+	// InstanceType to launch
+	MachineType string
+
+	// Zone - to deploy into
+	Zone string
+
+	// Pubkey to use for the instance access
+	KeyBody string
+}
+
+type AWSInstanceParams struct {
+	// The template id to use in order to launch an instance
+	LaunchTemplateID string
+
+	// ami of the instance will be launched from
+	AMI string
+
+	// InstanceType to launch
+	InstanceType types.InstanceType
+
+	// Zone - to deploy into
+	Zone string
+
+	// Pubkey to use for the instance access
+	KeyName string
+
+	// UserData for the instance launch
+	UserData []byte
+}
