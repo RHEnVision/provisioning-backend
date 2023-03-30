3bbe0fe703e323c0b598c543cabbfc8cd477f144
Lukas Zapletal <lzap+git@redhat.com>
2023-03-30 14:32:26 +0200

    refactor: move cache hit metrics into the proper package

--- internal/cache/metrics_decorator.go
+++ internal/cache/metrics_decorator.go
@@ -3,23 +3,8 @@ package cache
 import (
 	"context"
 
+	"github.com/RHEnVision/provisioning-backend/internal/metrics"
 	"github.com/RHEnVision/provisioning-backend/internal/models"
-	"github.com/RHEnVision/provisioning-backend/internal/version"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-)
-
-var (
-	accountIdHits = promauto.NewCounter(prometheus.CounterOpts{
-		Name:        "app_cache_account_hits",
-		Help:        "The total number of cache hits for account ID",
-		ConstLabels: prometheus.Labels{"service": version.PrometheusLabelName},
-	})
-	accountIdMisses = promauto.NewCounter(prometheus.CounterOpts{
-		Name:        "app_cache_account_miss",
-		Help:        "The total number of cache misses for account ID",
-		ConstLabels: prometheus.Labels{"service": version.PrometheusLabelName},
-	})
 )
 
 type accountMetricsCache struct {
@@ -36,11 +21,11 @@ func NewAccountDecorator(accountId AccountIdCache) *accountMetricsCache {
 func (c *accountMetricsCache) FindAccountId(ctx context.Context, OrgID, AccountNumber string) (*models.Account, error) {
 	value, err := c.accountId.FindAccountId(ctx, OrgID, AccountNumber)
 	if err != nil {
-		accountIdMisses.Inc()
+		metrics.IncCacheHit("account", "miss")
 		return nil, err
 	}
 
-	accountIdHits.Inc()
+	metrics.IncCacheHit("account", "hit")
 	return value, nil
 }
 
