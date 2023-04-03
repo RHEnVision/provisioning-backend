a3ba1a77bc6067fc65a90ae6c451aee7f9dd6be7
Lukas Zapletal <lzap+git@redhat.com>
2023-04-03 11:15:16 +0200

    chore: introduce Sentry replacer

--- internal/logging/sentry.go
+++ internal/logging/sentry.go
@@ -12,13 +12,18 @@ import (
 // sentryWriter creates a zerolog writer for sentry.
 // Uses github.com/archdx/zerolog-sentry which is very simple wrapper.
 func sentryWriter(dsn string) (io.Writer, func(), error) {
-	wr, err := sentrywriter.New(dsn)
+	sWriter, err := sentrywriter.New(dsn)
 	if err != nil {
 		return nil, func() {}, fmt.Errorf("cannot initialize sentry: %w", err)
 	}
 	sentry.ConfigureScope(func(scope *sentry.Scope) {
 		scope.SetTag("stream", config.BinaryName())
 	})
-	// close function returns error, but it's always nil, so we are ignoring it here
-	return wr, func() { _ = wr.Close() }, nil
+	fWriter := NewSentryReplacer(sWriter)
+
+	closeFunc := func() {
+		_ = fWriter.Close()
+		_ = sWriter.Close()
+	}
+	return fWriter, closeFunc, nil
 }
---
+++ internal/logging/sentry_replacer.go
@@ -0,0 +1,85 @@
+package logging
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+var filters = []string{
+	// Example (AWS SDK): RequestID: ca767444-d1f9-11ed-afa1-0242ac120002
+	`[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}`,
+	// Example (AWS SDK): arn:aws:iam::4328974392798432:role/my-role-123
+	`arn:aws:[[:word:]]+::\d+:[[:word:]\*-]+/[[:word:]\*-]+`,
+}
+
+var replacement = []byte{'?'}
+
+type SentryReplacer struct {
+	buf    []byte
+	w      io.Writer
+	re     *regexp.Regexp
+	mu     sync.Mutex
+	closed bool
+}
+
+func NewSentryReplacer(w io.Writer) *SentryReplacer {
+	sr := SentryReplacer{
+		w:  w,
+		re: regexp.MustCompile("(" + strings.Join(filters, "|") + ")"),
+	}
+	return &sr
+}
+
+func (sr *SentryReplacer) Write(p []byte) (n int, err error) {
+	sr.mu.Lock()
+	defer sr.mu.Unlock()
+
+	if sr.closed {
+		return 0, io.EOF
+	}
+
+	// Append p to our own buffer, see if there's anything we need to censor.
+	sr.buf = append(sr.buf, p...)
+
+	// If we've appended at least a line, censor it and write it out.
+	for {
+		if len(sr.buf) == 0 {
+			// Buffer flushed out completely.
+			return len(p), nil
+		}
+
+		idx := bytes.IndexRune(sr.buf, '\n')
+		if idx < 0 {
+			// No line yet, just lie to the caller and tell them we wrote p.
+			return len(p), nil
+		}
+
+		var line []byte
+		line, sr.buf = sr.buf[:idx+1], sr.buf[idx+1:]
+		line = sr.re.ReplaceAll(line, replacement)
+
+		_, err := sr.w.Write(line)
+		if err != nil {
+			// This is not strictly the error related to the incoming `p`, but the best we can do.
+			return 0, fmt.Errorf("cannot filter: %w", err)
+		}
+	}
+}
+
+func (sr *SentryReplacer) Close() error {
+	sr.mu.Lock()
+	defer sr.mu.Unlock()
+
+	replaced := sr.re.ReplaceAll(sr.buf, replacement)
+	_, err := sr.w.Write(replaced)
+	if err != nil {
+		return fmt.Errorf("cannot close filter: %w", err)
+	}
+
+	sr.closed = true
+	return nil
+}
