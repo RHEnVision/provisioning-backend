a9b23812df4e15083647cfca837836ee698ec71f
Lukáš Zapletal <lzap+git@redhat.com>
2023-05-30 09:38:18 +0200

    refactor: deprecate ctxval.Logger

--- internal/ctxval/logger.go
+++ internal/ctxval/logger.go
@@ -4,20 +4,20 @@ import (
 	"context"
 
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 // Logger returns the main logger with context fields or the standard global logger
-// when the main logger was not set. Never returns nil.
+// when the main logger was not set. When nil is passed, returns noop logger.
+// Never returns nil.
+//
+// Deprecated: Use zerolog.Ctx or log.Ctx alias instead.
 func Logger(ctx context.Context) *zerolog.Logger {
-	value := ctx.Value(loggerCtxKey)
-	if ctx == nil || value == nil {
-		return &log.Logger
-	}
-	return value.(*zerolog.Logger)
+	return zerolog.Ctx(ctx)
 }
 
 // WithLogger returns context copy with logger.
+//
+// Deprecated: Use log.Logger.WithContext(ctx) instead.
 func WithLogger(ctx context.Context, logger *zerolog.Logger) context.Context {
-	return context.WithValue(ctx, loggerCtxKey, logger)
+	return logger.WithContext(ctx)
 }
