a9e30b22780185b62f97ba6a1cff1b2a83504815
aabramov <aabramov@redhat.com>
2023-07-11 13:52:39 +0300

    refactor: Add GCP job test

--- internal/clients/stubs/errors.go
+++ internal/clients/stubs/errors.go
@@ -6,6 +6,7 @@ import (
 
 var (
 	NotImplementedErr            = errors.New("stub not yet implemented")
+	MissingInstanceIDErr         = errors.New("instance id is not present")
 	SourceAuthenticationNotFound = errors.New("stubbed authentication for source not found")
 	ContextReadError             = errors.New("failed to find or convert dao stored in testing context")
 )
--- internal/dao/stubs/reservation_dao.go
+++ internal/dao/stubs/reservation_dao.go
@@ -145,5 +145,10 @@ func (stub *reservationDaoStub) Delete(ctx context.Context, id int64) error {
 }
 
 func (stub *reservationDaoStub) UpdateReservationInstance(ctx context.Context, reservationID int64, instance *clients.InstanceDescription) error {
+	for _, instRes := range stub.instances[reservationID] {
+		if instRes.InstanceID == instance.ID {
+			instRes.Detail.PublicIPv4 = instance.PublicIPv4
+		}
+	}
 	return nil
 }
