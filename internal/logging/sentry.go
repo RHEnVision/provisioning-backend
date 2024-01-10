13a09d07b800706b7c5f40dac3144f28ac3c2295
Ondrej Ezr <oezr@redhat.com>
2024-01-10 21:15:42 +0100

    fix(HMS-3369): Implement replacing as beforeSend hook
    With implementing the replace functionality as Sentry hook,
    we can implement it bit more efficiently for only messages that are sent to sentry.
    
    It also allows us to use the sentry writer as LevelWriter and leverage WriteLevel function.
    WriteLevel is bit more streamlined as it allows to skip filtering when not needed.

--- internal/logging/sentry.go
+++ internal/logging/sentry.go
@@ -2,28 +2,25 @@ package logging
 
 import (
 	"fmt"
-	"io"
 
 	"github.com/RHEnVision/provisioning-backend/internal/config"
 	sentrywriter "github.com/archdx/zerolog-sentry"
 	"github.com/getsentry/sentry-go"
+	"github.com/rs/zerolog"
 )
 
 // sentryWriter creates a zerolog writer for sentry.
 // Uses github.com/archdx/zerolog-sentry which is very simple wrapper.
-func sentryWriter(dsn string) (io.Writer, func(), error) {
-	sWriter, err := sentrywriter.New(dsn)
+func sentryWriter(dsn string) (zerolog.LevelWriter, func(), error) {
+	replacer := NewSentryReplacer()
+
+	sWriter, err := sentrywriter.New(dsn, sentrywriter.WithBeforeSend(replacer.Replace))
 	if err != nil {
 		return nil, func() {}, fmt.Errorf("cannot initialize sentry: %w", err)
 	}
 	sentry.ConfigureScope(func(scope *sentry.Scope) {
 		scope.SetTag("stream", config.BinaryName())
 	})
-	fWriter := NewSentryReplacer(sWriter)
 
-	closeFunc := func() {
-		_ = fWriter.Close()
-		_ = sWriter.Close()
-	}
-	return fWriter, closeFunc, nil
+	return sWriter, func() { _ = sWriter.Close }, nil
 }
