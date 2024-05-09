b9b37812647a68c7c3364658f39d227433790afe
Lukas Zapletal <lzap+git@redhat.com>
2024-05-09 11:21:38 +0200

    chore: upgrade to 1.21
    Signed-off-by: Lukas Zapletal <lzap+git@redhat.com>

--- mk/0_initial.mk
+++ mk/0_initial.mk
@@ -7,7 +7,7 @@ PROJECT_DIR:=$(shell dirname $(abspath $(firstword $(MAKEFILE_LIST))))
 GIT_TAG?=$(shell git describe --tags --abbrev=0 2>/dev/null)
 
 # Update GitHub Workflows when changing this
-GO_VERSION?=1.20.10
+GO_VERSION?=1.21.9
 GOBIN:=$(PROJECT_DIR)/bin
 GO?=$(shell go env GOPATH)/bin/go$(GO_VERSION)
 GOROOT=$(shell $(GO) env GOROOT)
